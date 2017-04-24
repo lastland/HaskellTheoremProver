5dd78522280515c8611c67f6451aeca6efcb2f32
pritamChoudhury <r.choudhury1990@gmail.com>
2017-04-24 15:54:37 -0400

    Added Gentzen Style proofs

--- NatDedType.hs
+++ NatDedType.hs
@@ -614,3 +614,246 @@ wkPeircePP = ImpIP (ImpEP IdP
                  a0 = TrP
 
 -----------------------------------------------------------------------
+
+-----------------------------------------------------------------------
+          --Gentzen style cut-free sequent calculus (intuitionistic)
+-----------------------------------------------------------------------
+
+data DerivesGI (ctx :: [*]) a where
+
+  TrGI  :: -------------------------------------
+           DerivesGI ctx (Formula Top)
+
+  BotGI :: -----------------------------------------
+           DerivesGI (Formula Bot : ctx) (Formula a)
+  
+  IdGI  :: --------------------------------------
+           DerivesGI (Formula a : ctx) (Formula a)
+  
+  WkL   :: DerivesGI ctx (Formula a) ->
+           --------------------------------------
+           DerivesGI (Formula c : ctx) (Formula a)
+
+  CnL   :: DerivesGI (b : b : ctx) (Formula a) ->
+           ---------------------------------------
+           DerivesGI (b : ctx) (Formula a)
+        
+  ExL   :: DerivesGI ctx1 (Formula Top) ->
+           DerivesGI ctx2 (Formula Top) ->
+           DerivesGI '[b1] (Formula Top) ->
+           DerivesGI '[b2] (Formula Top) ->
+           DerivesGI (ctx1 ++ (b1 : b2 : ctx2)) (Formula a) ->
+           ----------------------------------------------------
+           DerivesGI (ctx1 ++ (b2 : b1 : ctx2)) (Formula a)
+        
+  AndR  :: DerivesGI ctx (Formula a1) ->
+           DerivesGI ctx (Formula a2) ->
+           ------------------------------------
+           DerivesGI ctx (Formula (a1 :/\: a2))
+          
+  AndL1 :: DerivesGI (Formula a1 : ctx) (Formula a) ->
+           -------------------------------------------------
+           DerivesGI (Formula (a1 :/\: a2) : ctx) (Formula a)
+  
+  AndL2 :: DerivesGI (Formula a2 : ctx) (Formula a) ->
+           -------------------------------------------------
+           DerivesGI (Formula (a1 :/\: a2) : ctx) (Formula a)
+  
+  OrR1  :: DerivesGI ctx (Formula a1) ->
+           -------------------------------------
+           DerivesGI ctx (Formula (a1 :\/: a2))
+  
+  OrR2  :: DerivesGI ctx (Formula a2) ->
+           --------------------------------------
+           DerivesGI ctx (Formula (a1 :\/: a2))
+  
+  OrL   :: DerivesGI (Formula a1 : ctx) (Formula a) ->
+           DerivesGI (Formula a2 : ctx) (Formula a) ->
+           ---------------------------------------------------
+           DerivesGI (Formula (a1 :\/: a2) : ctx) (Formula a)
+           
+  ImpR  :: DerivesGI (Formula a : ctx) (Formula b) ->
+           -------------------------------------------
+           DerivesGI ctx (Formula (a :~>: b))
+           
+  ImpL  :: DerivesGI ctx (Formula a) ->
+           DerivesGI (Formula b : ctx) (Formula c) ->
+           ------------------------------------------------
+           DerivesGI (Formula (a :~>: b) : ctx) (Formula c)
+
+
+-----------------------------------------------------------------
+           --Some Intuitionistic Theorems (Gentzen style proof)
+-----------------------------------------------------------------
+
+-- |- T    
+empDerTrG :: DerivesGI '[] (Formula Top)
+empDerTrG = TrGI
+
+-- p, q, r, s, ... |- r <=> q, p, r, s, ... |- r
+flipCtxG :: DerivesGI  (Formula p : Formula q : ctx) (Formula r) ->
+            DerivesGI  (Formula q : Formula p : ctx) (Formula r)
+flipCtxG t = ExL empDerTrG TrGI TrGI TrGI t
+
+
+-- p /\ (q \/ r) |- (p /\ q) \/ (p /\ r) (Gentzen style)
+distr1G :: DerivesGI '[Formula (p :/\: (q :\/: r))]
+                    (Formula ((p :/\: q) :\/: (p :/\: r)))
+distr1G = CnL (AndL1 (flipCtxG (AndL2 (OrL (OrR1 (AndR (flipCtxG IdGI) IdGI))
+                                       (OrR2 (AndR (flipCtxG IdGI) IdGI))))))
+
+-- (p /\ q) \/ (p /\ r) |- p /\ (q \/ r) (Gentzen Style)
+distr1'G :: DerivesGI '[Formula ((p :/\: q) :\/: (p :/\: r))]
+                      (Formula (p :/\: (q :\/: r)))
+distr1'G = OrL (AndR (AndL1 IdGI) (AndL2 (OrR1 IdGI)))
+               (AndR (AndL1 IdGI) (AndL2 (OrR2 IdGI)))
+
+-- ~(p \/ q) |- ~p /\ ~q (Gentzen Style)
+deMorIntG :: DerivesGI '[Formula ((p :\/: q) :~>: Bot)]
+                       (Formula ((p :~>: Bot) :/\: (q :~>: Bot)))
+deMorIntG = AndR (ImpR (flipCtxG (ImpL (OrR1 IdGI) IdGI)))
+                 (ImpR (flipCtxG (ImpL (OrR2 IdGI) IdGI)))
+
+-- ~p \/ q |- p -> q (Gentzen Style)
+orImpG :: DerivesGI '[Formula ((p :~>: Bot) :\/: q)]
+                    (Formula (p :~>: q))
+orImpG = OrL (ImpR (flipCtxG (ImpL IdGI BotGI))) (ImpR (flipCtxG IdGI))
+          
+-- |- ((((p -> q) -> p) -> p) -> q) -> q (Gentzen Style)
+wkPeirceG :: DerivesGI '[]
+                     (Formula (((((p :~>: q) :~>: p) :~>: p) :~>: q) :~>: q))
+wkPeirceG = ImpR (CnL (ImpL (ImpR (CnL (ImpL (ImpR
+                 (ExL a TrGI TrGI TrGI (flipCtxG
+                 (ImpL (ImpR (flipCtxG IdGI)) IdGI)))) IdGI))) IdGI))
+  where    a :: DerivesGI '[Formula p] (Formula Top)
+           a = TrGI
+
+  
+------------------------------------------------------------------
+         --Gentzen style cut-free sequent calculus (Classical)
+------------------------------------------------------------------
+
+           
+data DerivesGC (ctxl :: [*]) (ctxr :: [*])  where
+
+  TrGC  :: -------------------------------------
+           DerivesGC ctxl (Formula Top : ctxr)
+
+  BotGC :: -------------------------------------
+           DerivesGC (Formula Bot : ctxl) ctxr
+  
+  IdGC  :: -----------------------------------------------
+           DerivesGC (Formula a : ctxl) (Formula a : ctxr)
+  
+  WkLC  :: DerivesGC ctxl ctxr ->
+           -----------------------------------
+           DerivesGC (Formula a : ctxl) ctxr
+
+  WkRC  :: DerivesGC ctxl ctxr ->
+           -----------------------------------
+           DerivesGC ctxl (Formula a : ctxr)
+
+  CnLC  :: DerivesGC (b : b : ctxl) ctxr ->
+           ----------------------------------
+           DerivesGC (b : ctxl) ctxr
+
+  CnRC  :: DerivesGC ctxl (b : b : ctxr) ->
+           ----------------------------------
+           DerivesGC ctxl (b : ctxr)
+        
+  ExLC  :: DerivesGC ctx1 '[Formula Top] ->
+           DerivesGC ctx2 '[Formula Top] ->
+           DerivesGC '[b1] '[Formula Top] ->
+           DerivesGC '[b2] '[Formula Top] ->
+           DerivesGC (ctx1 ++ (b1 : b2 : ctx2)) ctxr ->
+           -----------------------------------------------
+           DerivesGC (ctx1 ++ (b2 : b1 : ctx2)) ctxr
+           
+  ExRC  :: DerivesGC '[Formula Bot] ctx1 ->
+           DerivesGC '[Formula Bot] ctx2 ->
+           DerivesGC '[Formula Bot] '[b1] ->
+           DerivesGC '[Formula Bot] '[b2] ->
+           DerivesGC ctxl (ctx1 ++ (b1 : b2 : ctx2)) ->
+           ------------------------------------------------
+           DerivesGC ctxl (ctx1 ++ (b2 : b1 : ctx2))
+        
+  AndRC :: DerivesGC ctxl (Formula a1 : ctxr) ->
+           DerivesGC ctxl (Formula a2 : ctxr) ->
+           --------------------------------------------
+           DerivesGC ctxl (Formula (a1 :/\: a2) : ctxr)
+          
+  AndL1C :: DerivesGC (Formula a1 : ctxl) ctxr ->
+            -----------------------------------------------
+            DerivesGC (Formula (a1 :/\: a2) : ctxl) ctxr
+  
+  AndL2C :: DerivesGC (Formula a2 : ctxl) ctxr ->
+            -----------------------------------------------
+            DerivesGC (Formula (a1 :/\: a2) : ctxl) ctxr
+  
+  OrR1C :: DerivesGC ctxl (Formula a1 : ctxr) ->
+           --------------------------------------------
+           DerivesGC ctxl (Formula (a1 :\/: a2) : ctxr)
+  
+  OrR2C :: DerivesGC ctxl (Formula a2 : ctxr) ->
+           --------------------------------------------
+           DerivesGC ctxl (Formula (a1 :\/: a2) : ctxr)
+  
+  OrLC  :: DerivesGC (Formula a1 : ctxl) (Formula a : ctxr) ->
+           DerivesGC (Formula a2 : ctxl) (Formula a : ctxr) ->
+           ---------------------------------------------------
+           DerivesGC (Formula (a1 :\/: a2) : ctxl) ctxr
+           
+  ImpRC :: DerivesGC (Formula a : ctxl) (Formula b : ctxr) ->
+           ---------------------------------------------------
+           DerivesGC ctxl (Formula (a :~>: b) : ctxr)
+           
+  ImpLC :: DerivesGC ctxl (Formula a : ctxr) ->
+           DerivesGC (Formula b : ctxl) ctxr ->
+           --------------------------------------------
+           DerivesGC (Formula (a :~>: b) : ctxl) ctxr
+
+
+----------------------------------------------------------------------
+        --Some classical Theorems (Gentzen style proof)
+----------------------------------------------------------------------
+
+-- |- T, ctxr    
+empDerTrGC :: DerivesGC '[] (Formula Top : ctxr)
+empDerTrGC = TrGC
+
+-- Bot, ctxl |- '[]
+contrDerEmp :: DerivesGC (Formula Bot : ctxl) '[]
+contrDerEmp = BotGC
+
+-- p, q, ctxl |- ctxr <=> q, p, ctxl |- ctxr
+flipCtxLC :: DerivesGC (Formula p : Formula q : ctxl) ctxr ->
+             DerivesGC (Formula q : Formula p : ctxl) ctxr
+flipCtxLC = ExLC empDerTrGC TrGC TrGC TrGC
+
+-- ctxl |- r, s, ctxr <=> ctxl |- s, r, ctxr
+flipCtxRC :: DerivesGC ctxl (Formula r : Formula s : ctxr) ->
+             DerivesGC ctxl (Formula s : Formula r : ctxr)
+flipCtxRC = ExRC contrDerEmp BotGC BotGC BotGC 
+
+-- |- p \/ ~p (Gentzen Style)
+lemG :: DerivesGC '[]
+                 '[Formula (p :\/: (p :~>: Bot))]
+lemG = CnRC (OrR1C (flipCtxRC (OrR2C (ImpRC (flipCtxRC IdGC)))))
+
+-- |- (((p -> q) -> p) -> p) -> q (Gentzen Style)
+peirceLG :: DerivesGC '[]
+                     '[Formula (((p :~>: q) :~>: p) :~>: p)]
+peirceLG = ImpRC (CnLC (ImpLC (ImpRC (flipCtxRC IdGC)) IdGC))
+   
+-- p -> q |- ~p \/ q (Gentzen Style)
+impOrG :: DerivesGC '[Formula (p :~>: q)]
+                    '[Formula ((p :~>: Bot) :\/: q)]
+impOrG = CnLC (ImpLC (flipCtxRC (OrR1C (ImpRC (flipCtxRC IdGC)))) (OrR2C IdGC))
+
+-- ~(p /\ q) |- ~p \/ ~q
+deMorG :: DerivesGC '[Formula ((p :/\: q) :~>: Bot)]
+                   '[Formula ((p :~>: Bot) :\/: (q :~>: Bot))]
+deMorG = CnRC (OrR1C (ImpRC (flipCtxRC (flipCtxLC (OrR2C
+         (ImpRC (flipCtxLC (ImpLC (AndRC (flipCtxLC IdGC) IdGC) BotGC))))))))
+
+------------------------------------------------------------------------------
