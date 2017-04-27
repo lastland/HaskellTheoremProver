6b6c8d33ef063fa80d2ae43e735134187efe03d3
Yao Li <hnkfliyao@gmail.com>
2017-04-27 19:49:19 -0400

    Reorganize Main.hs.

--- Main.hs
+++ Main.hs
@@ -24,6 +24,22 @@ import qualified Printing.Proof             as Proof
 import           Printing.ProofTree         (ProofTree, printTree, showTheorem)
 import qualified Printing.ProofTree         as ProofTree
 
+
+-- Run tests, then enter the main menu.
+main :: IO ()
+main = do
+  putStrLn "Welcome to our Theorem Prover!"
+  putStrLn "Let's start by running some tests:"
+  runTestTT FNDedTests.testInt
+  runTestTT FNDedTests.testInt
+  putStrLn "Tests completed."
+  mainMenu
+  putStrLn "Bye!"
+
+-------------------------------------------------------------------------------
+                   -- Theorems and Proofs to Show
+-------------------------------------------------------------------------------
+
 data Logic = Classic | Intuition
 
 instance Show Logic where
@@ -67,77 +83,80 @@ gentzenProofs = [(KGInt.pp KGIntTheorems.notTandF', Intuition),
                  (KGCl.pp KGClTheorems.doubNeg1', Classic),
                  (KGCl.pp KGClTheorems.excludedMiddle', Classic)]
 
-main :: IO ()
-main = do
-  putStrLn "Welcome to our Theorem Prover!"
-  putStrLn "Let's start by running some tests:"
-  runTestTT FNDedTests.testInt
-  runTestTT FNDedTests.testInt
-  putStrLn "Tests completed."
+-------------------------------------------------------------------------------
+                     -- All the Menus
+-------------------------------------------------------------------------------
+
+-- The main menu.
+mainMenu :: IO ()
+mainMenu = do
+  putSeparation "="
+  putStrLn "What would you like me to do?"
+  putStrLn "1: Print some proofs in natural deductions."
+  putStrLn "   (Propositions are implemented at data level in this version)"
+  putStrLn "2: Print some proofs in Gentzen style."
+  putStrLn "   (Propositions are implemented at type level in this version)"
+  putStrLn "x: Exit."
+  putStr "Please enter your command (1, 2, or x): "
+  hFlush stdout
+  c <- getLine
+  runCommandMainMenu c
+
+
+-- Execute user's command in the main menu.
+runCommandMainMenu :: String -> IO ()
+runCommandMainMenu "1" = showProofMenu nDedProofs
+runCommandMainMenu "2" = showProofMenu gentzenProofs
+runCommandMainMenu "x" = return ()
+runCommandMainMenu _ = do
+  putStrLn "Sorry. I don't understand your command. Please try again."
   mainMenu
-  putStrLn "Bye!"
-  where
-    -- The main menu.
-    mainMenu :: IO ()
-    mainMenu = do
-      putSeparation "="
-      putStrLn "What would you like me to do?"
-      putStrLn "1: Print some proofs in natural deductions."
-      putStrLn "   (Propositions are implemented at data level in this version)"
-      putStrLn "2: Print some proofs in Gentzen style."
-      putStrLn "   (Propositions are implemented at type level in this version)"
-      putStrLn "x: Exit."
-      putStr "Please enter your command (1, 2, or x): "
-      hFlush stdout
-      c <- getLine
-      runCommandMainMenu c
-    -- Execute user's command in the main menu.
-    runCommandMainMenu :: String -> IO ()
-    runCommandMainMenu "1" = showProofMenu nDedProofs
-    runCommandMainMenu "2" = showProofMenu gentzenProofs
-    runCommandMainMenu "x" = return ()
-    runCommandMainMenu _ = do
-      putStrLn "Sorry. I don't understand your command. Please try again."
-      mainMenu
-    -- The "show proof menu":
-    -- Users can choose to print the proof of a theorem from our theorem list,
-    -- or get back to the main menu, or exit our program.
-    -- Notice that this function is shared by
-    -- the part that prints data-level theorems with proofs
-    -- in natural deduction styles,
-    -- and the part that prints type-level theorems with proofs
-    -- in Gentzen style.
-    showProofMenu :: [(ProofTree, Logic)] -> IO ()
-    showProofMenu pfs = do
-      putSeparation "-"
-      putStrLn "Which proof would you like me to print?"
-      mapM_ putStrLn (theoremsToShow pfs)
-      putStrLn "q: get back to previous menu."
-      putStrLn "x: Exit."
-      putStr "Please enter your command: "
-      hFlush stdout
-      c <- getLine
-      runCommandShowProofMenu pfs c
-    -- Execute the command in the show proof menu.
-    -- Print the `ProofTree` if the command is a valid number.
-    runCommandShowProofMenu :: [(ProofTree, Logic)] -> String -> IO ()
-    runCommandShowProofMenu _ "x" = return ()
-    runCommandShowProofMenu _ "q" = mainMenu
-    runCommandShowProofMenu pfs x =
-      (case (readMaybe x :: Maybe Int) of
-        Just i ->
-          if i <= length pfs then
-            printTree $ fst (pfs !! (i - 1))
-            else putStrLn "Invalid number."
-        Nothing ->
-          putStrLn $ "Sorry. I don't understand your command." ++
-          " Please try again.") >>
-     showProofMenu pfs
-    -- Takes a list of `ProofTree``, show the proved theorems
-    theoremsToShow :: [(ProofTree, Logic)] -> [String]
-    theoremsToShow ps = [show i ++ ": " ++ showTheorem t ++
-                          " (" ++ show l ++ ")" |
-                          (i, (t, l)) <- zip [1..] ps]
-    -- Put some separation symbols on the screen
-    putSeparation :: String -> IO ()
-    putSeparation c = replicateM_ 80 (putStr c) >> putStrLn ""
+
+
+-- The "show proof menu":
+-- Users can choose to print the proof of a theorem from our theorem list,
+-- or get back to the main menu, or exit our program.
+-- Notice that this function is shared by
+-- the part that prints data-level theorems with proofs
+-- in natural deduction styles,
+-- and the part that prints type-level theorems with proofs
+-- in Gentzen style.
+showProofMenu :: [(ProofTree, Logic)] -> IO ()
+showProofMenu pfs = do
+  putSeparation "-"
+  putStrLn "Which proof would you like me to print?"
+  mapM_ putStrLn (theoremsToShow pfs)
+  putStrLn "q: get back to previous menu."
+  putStrLn "x: Exit."
+  putStr "Please enter your command: "
+  hFlush stdout
+  c <- getLine
+  runCommandShowProofMenu pfs c
+
+
+-- Execute the command in the show proof menu.
+-- Print the `ProofTree` if the command is a valid number.
+runCommandShowProofMenu :: [(ProofTree, Logic)] -> String -> IO ()
+runCommandShowProofMenu _ "x" = return ()
+runCommandShowProofMenu _ "q" = mainMenu
+runCommandShowProofMenu pfs x =
+  (case (readMaybe x :: Maybe Int) of
+    Just i ->
+      if i <= length pfs then
+        printTree $ fst (pfs !! (i - 1))
+        else putStrLn "Invalid number."
+    Nothing ->
+      putStrLn $ "Sorry. I don't understand your command." ++
+      " Please try again.") >>
+ showProofMenu pfs
+
+
+-- Takes a list of `ProofTree``, show the proved theorems
+theoremsToShow :: [(ProofTree, Logic)] -> [String]
+theoremsToShow ps = [show i ++ ": " ++ showTheorem t ++
+                      " (" ++ show l ++ ")" |
+                      (i, (t, l)) <- zip [1..] ps]
+
+-- Put some separation symbols on the screen
+putSeparation :: String -> IO ()
+putSeparation c = replicateM_ 80 (putStr c) >> putStrLn ""
